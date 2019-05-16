d5eaf4310f8d322d1782110b02b43cd02ed14ea2
EgorAB <enterskylife@gmail.com>
2019-05-16 18:53:41 +0300

    :sunglasses:

--- app/src/main/java/com/example/flacplayer/MainActivity.kt
+++ app/src/main/java/com/example/flacplayer/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.flacplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.animation.AlphaAnimation
+
+
 
 class MainActivity : AppCompatActivity() {
-
+    private val buttonClick = AlphaAnimation(1f, 0.8f)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toolBar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
