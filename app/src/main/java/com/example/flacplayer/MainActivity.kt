60d927f57c579e24f83dc3bcc181130c13a5bbe7
EgorAB <enterskylife@gmail.com>
2019-05-17 12:34:04 +0300

    :smirk:

--- app/src/main/java/com/example/flacplayer/MainActivity.kt
+++ app/src/main/java/com/example/flacplayer/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.flacplayer
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.library_scene.*
+import kotlinx.android.synthetic.main.play_scene.*
+import kotlinx.android.synthetic.main.playlist_scene.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +27,28 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         bottomNavigationView.selectedItemId = R.id.playButton
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.playButton -> {
+                    play_scene.visibility = View.VISIBLE
+                    playlist_scene.visibility = View.GONE
+                    library_scene.visibility = View.GONE
+                    true
+                }
+                R.id.playlistButton -> {
+                    play_scene.visibility = View.GONE
+                    playlist_scene.visibility = View.VISIBLE
+                    library_scene.visibility = View.GONE
+                    true
+                }
+                R.id.libraryButton -> {
+                    play_scene.visibility = View.GONE
+                    playlist_scene.visibility = View.GONE
+                    library_scene.visibility = View.VISIBLE
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
