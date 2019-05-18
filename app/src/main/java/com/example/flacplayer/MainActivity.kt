e956ab58d9c99728cad14d91e0cf3d20988f6205
EgorAB <enterskylife@gmail.com>
2019-05-18 03:30:17 +0300

    :neutral_face:

--- app/src/main/java/com/example/flacplayer/MainActivity.kt
+++ app/src/main/java/com/example/flacplayer/MainActivity.kt
@@ -1,54 +1,88 @@
 package com.example.flacplayer
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.transition.Fade
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.library_scene.*
+import kotlinx.android.synthetic.main.options_scene.*
 import kotlinx.android.synthetic.main.play_scene.*
 import kotlinx.android.synthetic.main.playlist_scene.*
 
 class MainActivity : AppCompatActivity() {
+    private var playerActive = false
+    private var selectedScene: View? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val toolBar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolBar)
-        toolbar.title = "Воспроизведение аудиозаписи"
-        supportActionBar?.setDisplayShowTitleEnabled(true)
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        selectedScene = play_scene
         bottomNavigationView.selectedItemId = R.id.playButton
-        bottomNavigationView.setOnNavigationItemSelectedListener{
-            when(it.itemId){
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
                 R.id.playButton -> {
-                    play_scene.visibility = View.VISIBLE
-                    playlist_scene.visibility = View.GONE
-                    library_scene.visibility = View.GONE
+                    selectScene(play_scene)
                     true
                 }
                 R.id.playlistButton -> {
-                    play_scene.visibility = View.GONE
-                    playlist_scene.visibility = View.VISIBLE
-                    library_scene.visibility = View.GONE
+                    selectScene(playlist_scene)
                     true
                 }
                 R.id.libraryButton -> {
-                    play_scene.visibility = View.GONE
-                    playlist_scene.visibility = View.GONE
-                    library_scene.visibility = View.VISIBLE
+                    selectScene(library_scene)
+                    true
+                }
+                R.id.moreButton -> {
+                    selectScene(options_scene)
                     true
                 }
                 else -> true
             }
         }
+        playButton.setOnClickListener {
+            if (playerActive) pause()
+            else play()
+        }
+
+        sampleButton.setOnClickListener {
+            var tr = Fade()
+            TransitionManager.beginDelayedTransition(sampleButtonContainer, Slide(Gravity.START))
+        }
+    }
+
+    fun selectScene(newScene:View) {
+        selectedScene?.visibility = View.GONE
+        selectedScene = newScene
+        selectedScene?.visibility = View.VISIBLE
+    }
+
+    private fun play() {
+        playerActive = true
+        playButton.setImageResource(R.drawable.ic_pause_black_24dp)
+    }
+
+    private fun pause() {
+        playerActive = false
+        playButton.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.nav_settings -> selectScene(options_scene)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
