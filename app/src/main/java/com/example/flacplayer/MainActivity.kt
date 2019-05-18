9d986dbeccdf33f6fb0236d27937032dee9f4399
EgorAB <enterskylife@gmail.com>
2019-05-18 22:22:58 +0300

    :skull:

--- app/src/main/java/com/example/flacplayer/MainActivity.kt
+++ app/src/main/java/com/example/flacplayer/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.flacplayer
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.text.Layout
 import android.transition.Fade
 import android.transition.Slide
 import android.transition.TransitionManager
-import android.view.Gravity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.blocks_scene.*
 import kotlinx.android.synthetic.main.library_scene.*
 import kotlinx.android.synthetic.main.options_scene.*
 import kotlinx.android.synthetic.main.play_scene.*
 import kotlinx.android.synthetic.main.playlist_scene.*
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
+
 
 class MainActivity : AppCompatActivity() {
     private var playerActive = false
@@ -51,14 +60,42 @@ class MainActivity : AppCompatActivity() {
             if (playerActive) pause()
             else play()
         }
-
         sampleButton.setOnClickListener {
             var tr = Fade()
             TransitionManager.beginDelayedTransition(sampleButtonContainer, Slide(Gravity.START))
         }
+        addTextBlockButton.setOnClickListener {
+            val view = LayoutInflater.from(this).inflate(
+                R.layout.text_block, null, false
+            )
+            blocks_scene.addView(view)
+            ObjectAnimator.ofInt(play_scene, "scrollY", parent_of_blocks_scene.bottom).setDuration(1400).start()
+            view.findViewWithTag<ImageButton>("delete_button").setOnClickListener {
+                view.animate().setDuration(500).alpha(0F).withEndAction {
+                    run { blocks_scene.removeView(view) }
+                }
+            }
+            view.findViewWithTag<ImageButton>("edit_button").setOnClickListener {
+                view.findViewWithTag<EditText>("edit_text").visibility = View.VISIBLE
+                view.findViewWithTag<EditText>("edit_text").setSelection(view.findViewWithTag<EditText>("edit_text").text.length)
+                view.findViewWithTag<EditText>("edit_text").setText(view.findViewWithTag<TextView>("text_view").text)
+                view.findViewWithTag<TextView>("text_view").visibility = View.GONE
+                view.findViewWithTag<ImageButton>("edit_button").visibility = View.GONE
+                view.findViewWithTag<ImageButton>("ok_button").visibility = View.VISIBLE
+            }
+            view.findViewWithTag<ImageButton>("ok_button").setOnClickListener {
+                view.findViewWithTag<TextView>("text_view").visibility = View.VISIBLE
+                view.findViewWithTag<TextView>("text_view").text = view.findViewWithTag<EditText>("edit_text").text
+                view.findViewWithTag<EditText>("edit_text").visibility = View.GONE
+                view.findViewWithTag<ImageButton>("ok_button").visibility = View.GONE
+                view.findViewWithTag<ImageButton>("edit_button").visibility = View.VISIBLE
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.findViewWithTag<EditText>("edit_text").windowToken, 0)
+            }
+        }
     }
 
-    fun selectScene(newScene:View) {
+    private fun selectScene(newScene: View) {
         selectedScene?.visibility = View.GONE
         selectedScene = newScene
         selectedScene?.visibility = View.VISIBLE
